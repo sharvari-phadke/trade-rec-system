32c164a8feafa511f2b5ee8e23891c9cf831efed
sharvari-phadke <114156408+sharvari-phadke@users.noreply.github.com>
2022-09-25 22:23:49 +0530

    Add files via upload

--- src/main/java/com/citi/system/utils/QueryUtils.java
+++ src/main/java/com/citi/system/utils/QueryUtils.java
@@ -16,14 +16,22 @@ public class QueryUtils {
 		case "getAllStocks":
 			sqlQuery= "select * from savedstocks where username= '"+params.get("username").toString()+"'";
 			break;
+		case "getStock":
+			sqlQuery= "select * from savedstocks where username= '"+params.get("username").toString()+"' and symbol = '"+params.get("symbol").toString()+"'";
 		case "deleteStock":
 			sqlQuery= "delete from savedstocks where username= '"+params.get("username").toString()+"' and symbol = '"+params.get("symbol").toString()+"'";
 			break;
 		case "addStock":
-			sqlQuery= "insert into savedstocks values ('"+params.get("symbol").toString()+"',"
-						+ params.get("currPrice")+", "+ params.get("close2WeeksAgo")+", "+ params.get("changePercent")+", "
-						+ params.get("quantitySaved")+", '"+ params.get("username")+"' )";
+			sqlQuery= "insert into savedstocks (symbol, currPrice, close2WeeksAgo, changePercent, quantitySaved, username ) values ( '"
+						+ params.get("symbol").toString()+"',"+ params.get("currPrice")+", "+ params.get("close2WeeksAgo")+", "
+						+ params.get("changePercent")+", "+ params.get("quantitySaved")+", '"+ params.get("username")+"' )";
 			break;
+		case "updateStock":
+			sqlQuery= "update savedstocks set currPrice="+ params.get("currPrice")+", close2WeeksAgo= "+ params.get("close2WeeksAgo")
+					+", changePercent= " + params.get("changePercent")+", quantitySaved= "+ params.get("quantitySaved")
+					+" where username= '"+params.get("username").toString()+"' and symbol = '"+params.get("symbol").toString()+"'";
+			break;
+		
 		}
 		return sqlQuery;
 	}
